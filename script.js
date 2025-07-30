608b29d996349659894dfcbb9bd557b13dc65348
giselleS08 <giselle.sarabia8@gmail.com>
2025-07-30 22:26:17 +0000

    collectionGame

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* VARIABLES */
 let catcher, fallingObject;
+let score = 0;
 
 /* SETUP RUNS ONCE */
 function setup() {
@@ -9,37 +10,63 @@ function setup() {
   //Create the first sprite 
   
   fallingObject = new Sprite(100,0,10);
-  fallingObject.vel.y=2;
+  fallingObject.vel.y=3;
 
 
-  catcher = new Sprite(200,300,100,20);
+  catcher = new Sprite(200,300,100,20, "k");
 }
 
 /* DRAW LOOP REPEATS */
 function draw() {
   background(255,255,237);
-
+  
+  
   
     if(fallingObject.y >= 410)
     {
       fallingObject.y=-20;
       fallingObject.x= random(10,390);
       fallingObject.vel.y=random(1,5);
+      score -=1;
     }
 
   //Move Catcher
-
   if (kb.pressing("left"))
+    {
+      catcher.vel.x = -5;
+      print("left");
+    }
+    else if (kb.pressing("right"))
+    {
+      catcher.vel.x = 5;
+      print("right");
+    } 
+    else
+    {
+      catcher.vel.x = 0;
+    }
+
+  if (catcher.x <= 50)
   {
-    catcher.vel.x = -3;
+    catcher.x = 50;
   }
-  else if (kb.pressing("right"))
+  else if (catcher.x >= 350)
   {
-    catcher.vel.x = 3;
-    
-  } 
-  else
-  {
-    catcher.vel.x = 0;
+    catcher.x = 350;
   }
+  
+  if (fallingObject.collides(catcher))
+  {
+    fallingObject.y=-20;
+    fallingObject.x= random(10,390);
+    fallingObject.vel.y=random(1,5)
+    fallingObject.direction = "down";
+    score +=1;
+  }
+
+
+
+  //score
+  fill(0);
+  text("Score: " + score, 5, 15);
 }
